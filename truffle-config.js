cb42c1f091cb0823c85c5e77a83e78f7550e15f1
codemonk22 <105297413+codemonk22@users.noreply.github.com>
2022-05-27 00:45:21 +0000

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -13,16 +13,9 @@ module.exports = {
       network_id: "*",       // Any network (default: none)
     },
 
-    // develop: {   // Localhost (default: none)
-    //   port: 7545,            // Standard Ethereum port (default: none)
-    //   network_id: "*",       // Any network (default: none)
-    // },
-
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.MNEMONIC_PHRASE,
         `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`),
-      // numberOfAddresses: 1,
-      //from: '0x87906b0E252B370251E58730D7B88FdEEC9d9d8e',
       from: '0xBb108964af881f000EE929bde6881D29a244Ee9B',
       network_id: 4,
       shareNonce: true,
@@ -31,7 +24,6 @@ module.exports = {
       timeoutBlocks: 200
       ,
     },
-
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -43,14 +35,6 @@ module.exports = {
   compilers: {
     solc: {
       version: "0.8.9",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
     }
   }
 }
